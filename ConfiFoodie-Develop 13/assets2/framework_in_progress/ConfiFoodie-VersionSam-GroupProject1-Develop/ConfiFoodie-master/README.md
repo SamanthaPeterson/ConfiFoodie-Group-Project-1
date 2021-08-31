8823660b63a59aeafe2c3e429b1d9031920c43a8
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-08-30 18:11:27 -0600

    Final Project

---
+++ ConfiFoodie-Develop 13/assets2/framework_in_progress/ConfiFoodie-VersionSam-GroupProject1-Develop/ConfiFoodie-master/README.md
@@ -0,0 +1,11 @@
+# No More Comfort Food Indecision! 
+
+## Objective 
+ConfiFoodie is designed to take the stress away from deciding what to make for the users next meal using real-time weather data and pairing it with correllating comfort-inspired recipes. Our goal is to provide a worry-free way to chill and recharge while also introducing users to new or forgotten comfort meal favorites.
+
+## Usage
+Users will first enter location details on the UI that utilizes Bulma and Modals, and then will be provided their weather related comfort food recipe via Javascript that utilizes both a weather and recipe API.
+
+## UI View
+
+## Link
---
+++ assets/resources/Scripts/js/weather.js
@@ -0,0 +1,35 @@
+// var weather;
+
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&appid=d196b19dfff57ff2ede0751c52d064e0';
+var units = '&units=imperial';
+var input = document.querySelector("#city");
+var searchField = document.querySelector("#searchField");
+
+function setup() {
+
+    var button = select('#submit');
+    button.mousePressed(findWeather);
+    searchField.addEventListener("submit", function (event) {
+        event.preventDefault();
+    })
+}
+
+
+document.getElementById('searchButton').addEventListener('click', findWeather)
+
+function findWeather(event) {
+    event.preventDefault();
+    input = input.value;
+    url = url + input + apiKey + units;
+    console.log(url);
+    fetch(url)
+        // api.openweathermap.org/data/2.5/weather?q={city}&appid={ }    
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.main.temp);
+            var temp = (data.main.temp)
+            var tempWeather = document.querySelector('#weatherTemp');
+            tempWeather.innerText = temp;
+        })
+}
